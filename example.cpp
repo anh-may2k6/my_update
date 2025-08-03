d522ac563b3a11936618f497fdf45ebb88f3fcbd
anh-may2k6 <hoangquocanh2n6@gmail.com>
2025-08-03 17:19:15 +0700

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -17,37 +17,37 @@ struct student{
 
 int calculate_age(const string &birth){
     int d,m,y;
-    char slash;
-    stringstream ss(birth);
-    ss >> d >> slash >> m >> slash >> y;
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
+    char slash; // gán để loại bỏ dấu gạch chéo
+    stringstream ss(birth); // tách từng string 
+    ss >> d >> slash >> m >> slash >> y; // lấy ngày tháng năm sinh ra gán kiểu int
+    time_t now = time(0); // lấy time hiện tại
+    tm *ltm = localtime(&now); // gọi 1 con trỏ có struct sẵn 
 
-    int cur_day = ltm->tm_mday;
-    int cur_moth = 1 + ltm->tm_mon;
-    int cur_year = 1900 + ltm->tm_year;
+    int cur_day = ltm->tm_mday; //tm_mday là ngày trong tháng 
+    int cur_moth = 1 + ltm->tm_mon; //tm_mon là tháng trong năm (mặc đinh là từ 0-11)
+    int cur_year = 1900 + ltm->tm_year; //tm_year là năm từ 1900 đổ lên
 
     int age = cur_year - y;
 
     if(cur_moth < m || (cur_moth == m && cur_day < d)){
-        age--;
+        age--; // chưa đủ tuổi thì trừ
     }
 
     return age;
 }
 
 bool cmp(const student &a,const student &b){
-    if(a.age != b.age) return a.age > b.age;
-    return a.id < b.id;
+    if(a.age != b.age) return a.age > b.age; // sắp xếp theo độ tuổi lớn nhất
+    return a.id < b.id; // sắp xếp theo id từ bé đến lớn
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(false); // loại bỏ liên kết giữa cin và scanf để cho code được chạy nhanh hơn
+    cin.tie(nullptr); // khi cin , ko cần cout phải đợi 
     int n;
     cin >> n;
-    cin.ignore();
-    vector<student> sv(n);
+    cin.ignore(); // loại bỏ dấu \n ra khỏi bộ nhớ đệm
+    vector<student> sv(n); // n vector sinh viên
     for(int i=0;i<n;i++){
         getline(cin , sv[i].id);
         getline(cin , sv[i].name);
